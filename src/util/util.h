eb0d28e145a65a4b374e1c4b9780651569c17177
첨단기술연수소/삼성전자 <CO_hrdc01.ID@partner.samsung.com>
2023-02-14 16:44:32 +0900

    Remove util files

--- src/util/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H_
-#define UTIL_H_
-#include <string>
-
-std::string GetHashCode(const std::string &password);
-std::string GetNonce();
-std::string GetPasswordWithNonce(const std::string &password, const std::string &nounce);
-
-#endif
