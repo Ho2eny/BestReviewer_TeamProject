bd78b0e50b85799c18cc20632826c5dc66c3f241
첨단기술연수소/삼성전자 <CO_hrdc01.ID@partner.samsung.com>
2023-02-14 14:29:53 +0900

    HttpClient first draft

---
+++ src/response.h
@@ -0,0 +1,23 @@
+#ifndef  _RESPONSE_H_ 
+#define  _RESPONSE_H_ 
+
+#include <string>
+
+using std::string;
+
+// TODO
+class Response {
+public:
+  Response() {}
+  Response(int status_code, const string &body);
+  ~Response();
+
+  int GetStatusCode() const { return 0; } 
+  string GetBody() const { return ""; }
+
+private:
+  int status_code;
+  string body;
+};
+
+#endif
