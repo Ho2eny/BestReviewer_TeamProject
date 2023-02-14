5e2a48be455390e544e1503870a372963a700162
첨단기술연수소/삼성전자 <CO_hrdc01.ID@partner.samsung.com>
2023-02-14 16:52:06 +0900

    Test Hash 함수를 api_test 에 추가

--- test/api_test.cc
+++ test/api_test.cc
@@ -10,6 +10,37 @@
 
 using namespace std;
 
+
+string GetHashCode(const string &password)
+{
+    long long hash = 5381;
+    int c;
+    const char *c_str = password.c_str();
+    while (c = *c_str++)
+    {
+        hash = (((hash << 5) + hash) + c) % 1000000007;
+    }
+    long long result = hash % 1000000007;
+
+    stringstream stream;
+    stream << result;
+    return stream.str();
+}
+
+string GetNonce()
+{
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<mt19937::result_type> dist9(1, 9);
+    return to_string(dist9(rng));
+}
+
+string GetPasswordWithNonce(const string &password, const string &nonce)
+{
+    string new_password = GetHashCode(password);
+    return GetHashCode(new_password + nonce);
+}
+
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     cout << "WriteCallback " << endl;
