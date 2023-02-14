e1303b5001819382709e6a54aa2092c906944e77
in.heo <in.heo@samsung.com>
2023-02-14 15:12:44 +0900

    Json 관련 Exception을 추가합니다
    - InvalidJsonStringException : JsonString이 Parsable 하지 않은 string일
    경우 예외를 발생시킵니다.
    - InvalidJsonSerializerException : JsonSerializer가 올바르지 않은 경우에
    예외를 발생시킵니다

---
+++ src/common/exception/json/Invalid_json_string_exception.h
@@ -0,0 +1,11 @@
+#ifndef INVALID_JSON_STRING_EXCEPTION_H_
+#define INVALID_JSON_STRING_EXCEPTION_H_
+
+#include "common/exception/base_exception.h"
+
+class InvalidJsonStringException : public BaseException {
+public:
+  InvalidJsonStringException(const char* message) : BaseException(message) {}
+};
+
+#endif 
