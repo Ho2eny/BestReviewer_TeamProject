21558efe64d4c0b08aa9d274cae98833ba104e1a
in.heo <in.heo@samsung.com>
2023-02-14 16:09:32 +0900

    빌드 에러를 수정합니다

--- src/common/exception/json/invalid_json_serializer_exception.h
+++ src/common/exception/json/invalid_json_serializer_exception.h
@@ -1,7 +1,7 @@
 #ifndef INVALID_JSON_SERIALIZER_EXCEPTION_H_
 #define INVALID_JSON_SERIALIZER_EXCEPTION_H_
 
-#include "common/exception/base_exception.h"
+#include "../base_exception.h"
 
 class InvalidJsonSerializerException : public BaseException {
 public:
--- src/interface/json_serializer.h
+++ src/interface/json_serializer.h
@@ -5,44 +5,44 @@
 #include <memory>
 #include <string>
 
-#include "common/exception/json/invalid_json_serializer_exception.h"
-#include "common/exception/json/Invalid_json_string_exception.h"
+#include "../common/exception/json/invalid_json_serializer_exception.h"
+#include "../common/exception/json/Invalid_json_string_exception.h"
 
 class JsonSerializer {
 public:
   JsonSerializer() {
     reader_builder_ = std::make_shared<Json::CharReaderBuilder>();
-    reader_ = reader_builder_->newCharReader();
-
     writer_builder_ = std::make_shared<Json::StreamWriterBuilder>();
   }
 
   virtual ~JsonSerializer() {
     reader_builder_.reset();
-    reader_.reset();
-
     writer_builder_.reset();
   }
 
-  static Json::Value ParseJson(const std::string& json) const;
-  static std::string ToSting(const Json::Value& json) const;
+  Json::Value ParseJson(const std::string& json) const;
+  std::string ToString(const Json::Value& json) const;
 
-private:
+protected:
   std::shared_ptr<Json::CharReaderBuilder> reader_builder_;
-  std::shared_ptr<Json::CharReader> reader_;
-
   std::shared_ptr<Json::StreamWriterBuilder> writer_builder_;
 };
 
-static inline Json::Value JsonSerializer::ParseJson(const std::string& json) const {
-  if (reader_ == nullptr) {
-    throw InvalidJsonSerializerException("CharReader is invalid");
+inline Json::Value JsonSerializer::ParseJson(const std::string& json) const {
+  if (reader_builder_ == nullptr) {
+    throw InvalidJsonSerializerException("ReaderBuilder is invalid");
   }
   
+  Json::CharReader* reader = reader_builder_->newCharReader();
+
+  if (reader == nullptr) {
+    throw InvalidJsonSerializerException("Failed to create CharReader with CharReaderBuilder");
+  }
+
   Json::Value json_object;
   std::string errors;
 
-  bool result = reader_->parse(json.c_str(), json.c_str() + json.size(), &json_object, &errors);
+  bool result = reader->parse(json.c_str(), json.c_str() + json.size(), &json_object, &errors);
 
   if (!result) {
     throw InvalidJsonStringException("Failed to parse json");
@@ -51,12 +51,12 @@ static inline Json::Value JsonSerializer::ParseJson(const std::string& json) con
   return json_object;
 }
 
-static inline std::string JsonSerializer::ToString(const Json::Value& json) const {
+inline std::string JsonSerializer::ToString(const Json::Value& json) const {
   if (writer_builder_ == nullptr) {
     throw InvalidJsonSerializerException("WriterBuilder is invalid");
   }
 
-  std::string json_str = Json::writeString(&writer_builder_.get(), json);
+  std::string json_str = Json::writeString(*writer_builder_.get(), json);
   
   return json_str;
 }
