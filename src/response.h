4b7a4454cef40c7ba51d9051e4d44a73bf37c99b
Seonah Moon <seonah1.moon@samsung.com>
2023-02-14 15:01:53 +0900

    Response: 생성자 및 getter 메소드 수정

--- src/response.h
+++ src/response.h
@@ -5,15 +5,15 @@
 
 using std::string;
 
-// TODO
 class Response {
 public:
-  Response() {}
-  Response(int status_code, const string &body);
+  Response() : status_code_(0), body_("") {}
+  Response(int status_code, const string &body)
+    : status_code_(status_code), body_(body) {}
   ~Response();
 
-  int GetStatusCode() const { return 0; } 
-  string GetBody() const { return ""; }
+  int GetStatusCode() const { return status_code_; } 
+  string GetBody() const { return body_; }
 
 private:
   int status_code_;
