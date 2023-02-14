798d592751385b1b16a05ccbbc25029b5b94c97a
chaejung45-lim <chaejung45.lim@samsung.com>
2023-02-14 07:56:31 +0000

    Change nonce and method

--- src/utils.h
+++ src/utils.h
@@ -12,33 +12,38 @@ using namespace std;
 
 class AuthorizationKey {
 public:
+    AuthorizationKey();
     void setId(const string& param) { id.assign(param); };
     void setPassword(const string& param) { password.assign(param); }
-    ll queryPassword();
+    string queryPassword();
+    string queryPasswordWithNonce();
+    string queryNonce() { return nonce; };
 private:
     string id, password, nonce;
     ll generateHash(const string& str);
 };
 
+AuthorizationKey::AuthorizationKey() {
+    srand((unsigned int)time(0));
+    //  temp  0~9 사이의 임의의 한자리 수   
+    nonce.assign(to_string(rand() % 9));
+}
+
 ll AuthorizationKey::generateHash(const string& str) {
     ll hash = 5381;
     int c;
     const char *c_str = str.c_str();
-    while(c = *c_str++) {
-        hash = ((( hash <<5 ) + hash) + c) % 1000000007;
-    }
+    while(c = *c_str++)
+        hash = ((( hash << 5 ) + hash) + c) % 1000000007;
     return hash % 1000000007;
 }
 
-ll AuthorizationKey::queryPassword() {
-    ll ret = 0;
-    string temp;
-    srand((unsigned int)time(0));
+string AuthorizationKey::queryPassword() {
+    return to_string(generateHash(password));
+}
 
-    //  temp  0~9 사이의 임의의 한자리 수   
-    nonce.assign(to_string(rand() % 9));
-    ret = generateHash( std::to_string(generateHash(password)) + nonce);
-    return ret;
+string AuthorizationKey::queryPasswordWithNonce() {
+    return to_string(generateHash( std::to_string(generateHash(password)) + nonce));
 }
 
 #endif
