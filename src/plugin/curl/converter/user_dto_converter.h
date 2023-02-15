4bb35772aef12701c7d00b8e57c8f7dc0d37d300
in.heo <in.heo@samsung.com>
2023-02-15 14:40:43 +0900

    Logout, Signup Dto를 HttpDto로 변환 로직 추가

--- src/plugin/curl/converter/user_dto_converter.h
+++ src/plugin/curl/converter/user_dto_converter.h
@@ -5,6 +5,11 @@
 
 #include "../../../interface/dto/user/login_response.h"
 #include "../../../interface/dto/user/login_request.h"
+#include "../../../interface/dto/user/logout_request.h"
+#include "../../../interface/dto/user/logout_response.h"
+#include "../../../interface/dto/user/signup_request.h"
+#include "../../../interface/dto/user/signup_response.h"
+
 #include "../../../response.h"
 #include "../../../request.h"
 
@@ -15,10 +20,17 @@ public:
   LoginResponse ConvertToLoginResponseFrom(const Response& http_response) const;
   Request ConvertToLoginHttpRequestFrom(const LoginRequest& login_request, const std::string& base_url) const;
 
+  Request ConvertToLogoutHttpRequestFrom(const LogoutRequest& logout_request, const std::string& base_url) const;
+
+  Request ConvertToSignupHttpRequestFrom(const SignupRequest& signup_request, const std::string& base_url) const;
+
 private:
   static const std::string kLoginEndpoint;
+  static const std::string kLogoutEndpoint;
+  static const std::string kSignupEndpoint;
 
   std::string ConvertToJsonString(const LoginRequest& login_request) const;
+  std::string ConvertToJsonString(const SignupRequest& login_request) const;
 
   std::shared_ptr<JsonSerializer> json_serializer_;
 };
