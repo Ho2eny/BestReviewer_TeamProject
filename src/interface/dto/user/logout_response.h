01bdca3ccf5b3c11d2b52636a28e9b26461b7dbd
in.heo <in.heo@samsung.com>
2023-02-15 14:23:18 +0900

    LogoutDto, SignupDto 추가합니다

---
+++ src/interface/dto/user/logout_response.h
@@ -0,0 +1,11 @@
+#ifndef LOGOUT_RESPONSE_H_
+#define LOGOUT_RESPONSE_H_
+
+#include "../base_response.h"
+
+class LogoutResponse : public BaseResponse {
+public:
+  LogoutResponse() {}
+};
+
+#endif
