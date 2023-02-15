84fc97b13ab5bc90f52d41b879eca1064e120fb5
in.heo <in.heo@samsung.com>
2023-02-15 14:23:43 +0900

    UserRepository API 업데이트합니다.

--- src/interface/repository/user_repository.h
+++ src/interface/repository/user_repository.h
@@ -2,11 +2,19 @@
 #define USER_REPOSITORY_H_
 
 #include "../dto/user/login_request.h"
-#include "../dto/User/login_response.h"
+#include "../dto/user/login_response.h"
+#include "../dto/user/logout_request.h"
+#include "../dto/user/logout_response.h"
+#include "../dto/user/signup_request.h"
+#include "../dto/user/signup_response.h"
 
 class UserRepository {
 public:
   virtual LoginResponse Login(const LoginRequest& request) = 0;
+
+  virtual SignupResponse Signup(const SignupRequest& request) = 0;
+
+  virtual LogoutResponse Logout(const LogoutRequest& request) = 0;
 };
 
 #endif
