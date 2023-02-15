7a6e6c7a64baf68dfbbafe05a7696c5622c3270b
Seonah Moon <seonah1.moon@samsung.com>
2023-02-15 18:09:02 +0900

    http_client_test: HttpPlugin을 사용하여 테스트

--- test/http_client_test.cc
+++ test/http_client_test.cc
@@ -3,12 +3,13 @@
 #include <json/json.h>
 #include <gtest/gtest.h>
 
-#include "../src/http/http_client.h"
+#include "../src/http/http_plugin.h"
 #include "../src/http/dto/request.h"
 #include "../src/http/dto/response.h"
 #include "../src/http/exception/network/authentication_failure_exception.h"
 #include "../src/http/exception/network/connection_failure_exception.h"
 #include "../src/http/exception/network/dns_resolving_failure_exception.h"
+#include "../src/http/thirdparty/curl/curl_client.h"
 
 using namespace std;
 
@@ -28,43 +29,40 @@ static string GetHashCode(const string &password)
     return stream.str();
 }
 
-class HttpClientTest : public ::testing::Test {
+class HttpPluginTest : public ::testing::Test {
     protected:
     void SetUp() override {
-        request = new Request("http://10.241.114.152:34568");
+        client = std::make_shared<CurlClient>();
+        request = std::make_shared<Request>("http://10.241.114.152:34568");
     }
     
     void TearDown() override {
-        delete request;
     }
 
-    Request *request;
+    shared_ptr<HttpPlugin> client;
+    shared_ptr<Request> request;
 };
 
-TEST_F(HttpClientTest, HttpGet)
+TEST_F(HttpPluginTest, HttpGet)
 {
-    HttpClient client;
-
     request->SetPath("/chat/welcome_arrayJson");
     
-    auto response = client.Get(*request);
+    auto response = client->Get(*request);
     EXPECT_EQ(200, response.GetStatusCode());
     EXPECT_EQ("", response.GetErrorMessage());
     EXPECT_TRUE(response.IsSuccess());
 } 
 
-TEST_F(HttpClientTest, HttpGetWithInvalidPath)
+TEST_F(HttpPluginTest, HttpGetWithInvalidPath)
 {
-    HttpClient client;
-
     request->SetPath("/1234");
     
-    auto response = client.Get(*request);
+    auto response = client->Get(*request);
     EXPECT_EQ(404, response.GetStatusCode());
     EXPECT_FALSE(response.IsSuccess());
 } 
 
-TEST_F(HttpClientTest, HttpPost)
+TEST_F(HttpPluginTest, HttpPost)
 {
     random_device dev;
     mt19937 rng(dev());
@@ -80,50 +78,42 @@ TEST_F(HttpClientTest, HttpPost)
     request->SetPath("/chat/account");
     request->SetBody(jsonData.c_str());
 
-
-    HttpClient client;
-    auto response = client.Post(*request);
+    auto response = client->Post(*request);
 
     EXPECT_EQ(200, response.GetStatusCode());
     EXPECT_EQ("", response.GetErrorMessage());
 } 
 
-TEST_F(HttpClientTest, HttpConnectionFailure)
+TEST_F(HttpPluginTest, HttpConnectionFailure)
 {
-    HttpClient client;
-
     Request invalid_request("http://1234");
 
-    EXPECT_THROW(client.Get(invalid_request), ConnectionFailureException);
+    EXPECT_THROW(client->Get(invalid_request), ConnectionFailureException);
 } 
 
-TEST_F(HttpClientTest, HttpDnsResolvingFailure)
+TEST_F(HttpPluginTest, HttpDnsResolvingFailure)
 {
-    HttpClient client;
-
     Request invalid_request("http://efef.efef.eff.com");
 
-    EXPECT_THROW(client.Get(invalid_request), DnsResolvingFailureException);
+    EXPECT_THROW(client->Get(invalid_request), DnsResolvingFailureException);
 } 
 
 #if 0
-TEST_F(HttpClientTest, HttpAuthenticationFailure)
+TEST_F(HttpPluginTest, HttpAuthenticationFailure)
 {
-    HttpClient client;
+    HttpPlugin client;
 
     Request invalid_request("https://jigsaw.w3.org/HTTP/Basic/");
     
-    EXPECT_THROW(client.Get(invalid_request), AuthenticationFailureException);
+    EXPECT_THROW(client->Get(invalid_request), AuthenticationFailureException);
 } 
 #endif
 
-TEST_F(HttpClientTest, HttpDelete)
+TEST_F(HttpPluginTest, HttpDelete)
 {
-    HttpClient client;
-
     request->SetPath("/chat/session?session_id=invalidsessionid");
    
-    auto response = client.Delete(*request);
+    auto response = client->Delete(*request);
     EXPECT_EQ(403, response.GetStatusCode());
     EXPECT_EQ("Not a valid session ID", response.GetBody());
 }
