dea06de1cb87cf6688a7cf43090c9115f218a2b6
Donghyuk Kim <donghyuk80.kim@samsung.com>
2023-02-16 14:12:18 +0900

    header 밖에서 선언되어 컴파일 에러를 유발하던 것들 수정하였습니다.

--- src/command/cache.h
+++ src/command/cache.h
@@ -37,14 +37,4 @@ private:
   std::vector<std::string> rooms_;
 };
 
-const std::string Cache::vID = "id";
-const std::string Cache::vTestID = "test_id";
-const std::string Cache::vTestNonce = "test_nonce";
-const std::string Cache::vTestPassword = "test_password";
-const std::string Cache::vBaseUrl = "base_url";
-const std::string Cache::vSessionID = "session_id";
-const std::string Cache::vTestSessionID = "test_session_id";
-const std::string Cache::vChatRoomName = "chat_room_name";
-const std::string Cache::vTestChatRoomName = "test_chat_room_name";
-
 #endif
